afe30db01571672d6b0cf17027705584601fe3f0
fishorfished <52461980+fishorfished@users.noreply.github.com>
2023-12-05 14:36:38 +0800

    Update human_detect_3.py parameters

--- human_detect_3.py
+++ human_detect_3.py
@@ -26,36 +26,59 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
 while True:
     success, img = cap.read()
     results = model(img, stream=True)
-
+    no_of_person = 0
+    detected = False
+    direction = 0
+    offset = 0
+    frame_filled = 0
+    other_person = 0
     # coordinates
     for r in results:
+        
         boxes = r.boxes
 
         for box in boxes:
-            # bounding box
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
-
-            # put box in cam
-            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-
-            # confidence
-            confidence = math.ceil((box.conf[0]*100))/100
-            print("Confidence --->",confidence)
-
-            # class name
+            
             cls = int(box.cls[0])
-            print("Class name -->", classNames[cls])
+            if (classNames[cls]=="person"):
+                # bounding box
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+                print(x1,y1,x2,y2)
 
-            # object details
-            org = [x1, y1]
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            fontScale = 1
-            color = (255, 0, 0)
-            thickness = 2
+                # put box in cam
+                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
-            cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+                # confidence
+                confidence = math.ceil((box.conf[0]*100))/100
+                print("Confidence --->",confidence)
 
+                # class name
+                cls = int(box.cls[0])
+                if (classNames[cls] == "person"):
+                    no_of_person =  no_of_person + 1
+                    if(other_person == 0):
+                        offset = (640/2 - (x1 + x2)/2)/(640)
+                        if (offset > 0):
+                            direction = 1
+                        frame_filled = (y2-y1)/(480)
+                    else:
+                        print("More than one person detected in same frame")
+
+                print("Class name -->", classNames[cls])
+
+                # object details
+                org = [x1, y1]
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                fontScale = 1
+                color = (255, 0, 0)
+                thickness = 2
+
+                cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+    if (no_of_person == 1):
+        detected = True
+    
+    print(detected, direction, offset, frame_filled)
     cv2.imshow('Webcam', img)
     if cv2.waitKey(1) == ord('q'):
         break
